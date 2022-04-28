f977ddfc785c72e7f21a3f4e1414d9c6c7168265
kkeeth <zensin0082@gmail.com>
2022-04-29 00:32:23 +0900

    chore: changed dist directory constructure

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     app: './src/index.js',
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/demo'),
     filename: '[name].bundle.js',
     clean: true,
   },
